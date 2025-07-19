d4013bc92d00731cbaaad6e6b16431bc8b93f965
chiaker <romabatanin2017@yandex.ru>
2025-07-19 22:37:18 +0300

    banned

--- src_b/api_v1/models.py
+++ src_b/api_v1/models.py
@@ -7,7 +7,7 @@ import hashlib
 
 DATABASE_URL = "sqlite:///./forum.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False}, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
