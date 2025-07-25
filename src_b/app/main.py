281532abcf685441af86d43d73c640a43b7374d5
chiaker <romabatanin2017@yandex.ru>
2025-07-25 22:32:52 +0300

    fix cache problem

--- src_b/app/main.py
+++ src_b/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, Request
 from fastapi.middleware.cors import CORSMiddleware
 from src_b.api_v1.views import router
 from src_b.api_v1.models import init_db
@@ -6,7 +6,19 @@ from fastapi.staticfiles import StaticFiles
 
 init_db()
 
+
+class NoCacheStaticFiles(StaticFiles):
+    async def get_response(self, path, scope):
+        response: Response = await super().get_response(path, scope)
+        response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0"
+        return response
+
+
 app = FastAPI()
+app.mount("/", NoCacheStaticFiles(directory="/home/root_server/forum/forum",
+          html=True), name="static")
+app.mount("/css", NoCacheStaticFiles(directory="/home/root_server/forum/forum/css"), name="css")
+
 
 app.add_middleware(
     CORSMiddleware,
