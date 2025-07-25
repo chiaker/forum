2cac522493ad03da6a24f30c638c690cd2221943
chiaker <romabatanin2017@yandex.ru>
2025-07-25 21:36:19 +0300

    added logs, still trying to fix

--- index.js
+++ index.js
@@ -92,6 +92,7 @@ const app = () => {
       `;
 
       item.querySelector("h2").addEventListener("click", (e) => {
+        console.log("Клик по теме:", topicId); // <-- ДОБАВЬ
         e.stopPropagation();
 
         const isOpen = state.openedPost === topicId;
